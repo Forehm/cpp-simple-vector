2d9f35b7bc10852245ff1eaba3cf77bcaa148240
Egor <112934317+Forehm@users.noreply.github.com>
2023-10-15 00:29:30 +0300

    Update array_ptr.h

--- simple-vector/array_ptr.h
+++ simple-vector/array_ptr.h
@@ -6,11 +6,8 @@
 template <typename Type>
 class ArrayPtr {
 public:
-    // Инициализирует ArrayPtr нулевым указателем
     ArrayPtr() = default;
 
-    // Создаёт в куче массив из size элементов типа Type.
-    // Если size == 0, поле raw_ptr_ должно быть равно nullptr
     explicit ArrayPtr(size_t size) {
         if (size != 0) {
             raw_ptr_ = new Type[size];
